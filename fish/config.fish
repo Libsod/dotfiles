763bfe302612f677700db33d90f83b9a9acbc7b8
qwislet <linuxuser.zlmug@slmail.me>
2024-02-18 00:49:45 +0400

    Deleted dotter and fzf fish plugin. The transition to Nix configuration has begun.

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,6 @@
 set fish_greeting ""
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-zoxide init fish | source
+zoxide init --cmd cd fish | source
 
 starship init fish | source
 
