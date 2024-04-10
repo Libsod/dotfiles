3d2b46d23690bfa445c5ab39e79b06f43ae8a1c5
qwislet <linuxuser.zlmug@slmail.me>
2024-04-11 01:00:54 +0400

    refactor(nvim): fully modified init.lua
    Added settings and mappings for neovide.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,66 @@
-if not vim.g.vscode then
-	require("core")
-	local nvim = require("core.highlights")
-	nvim.highlight_nvim()
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "nvchad.autocmds"
+require "autocmds"
+
+local function neovide_config()
+  vim.o.guifont = "Iosevka Nerd Font:h20"
+  vim.opt.linespace = 14
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_refresh_rate = 120
+  vim.g.neovide_no_idle = false
+  vim.g.neovide_refresh_rate_idle = 25
+  vim.g.neovide_profiler = false
+  vim.g.neovide_input_macos_alt_is_meta = false
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_cursor_vfx_particle_density = 10.0
+  vim.g.neovide_unlink_border_highlights = true
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
+neovide_config()
+
+vim.schedule(function()
+  require "mappings"
+end)
