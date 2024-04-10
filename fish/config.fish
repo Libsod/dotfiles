e20915a8a7c14f3acd9545af7d4456f7e651a547
qwislet <linuxuser.zlmug@slmail.me>
2024-04-11 00:46:04 +0400

    feat(fish): added new aliases
    Also changed fish file tracking (deleted useless files)

--- fish/config.fish
+++ fish/config.fish
@@ -6,20 +6,25 @@ starship init fish | source
 
 alias ll="exa -1 -l -a -g --icons --git"
 alias llt="exa -1 -a --icons --tree --level=2 --git-ignore"
-alias scf="source $HOME/.config/fish/config.fish"
+alias scf="source ~/.config/fish/config.fish"
 alias la="ls -la"
 alias bu="brew update && brew upgrade"
 alias v="nvim"
-alias vfsh="nvim $HOME/.config/fish/config.fish"
-alias vstrshp="nvim $HOME/.config/starship.toml"
-alias vwztrm="nvim $HOME/.wezterm.lua"
+alias nv="neovide --title-hidden"
+alias vfsh="nvim ~/.config/fish/config.fish"
+alias vstrshp="nvim ~/.config/starship.toml"
+alias vwztrm="nvim ~/.wezterm.lua"
 alias python="python3"
-alias nvm="mv $HOME/.config/nvim/ $HOME/.config/backupnvim2/ && mv $HOME/.config/primenvim/ $HOME/.config/nvim/"
 alias sshadd="ssh-add ~/.ssh/id_rsa"
 alias sshg="ssh -T git@github.com"
 alias cl="clear"
 alias clnvim="rm -rf ~/.local/share/nvim ~/.local/state/nvim ~/.cache/nvim"
 alias rmf="rm -rf"
+alias yab='echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai'
+alias yabupdate="yabai --stop-service && yabai --uninstall-service && brew upgrade yabai && yabai --start-service"
+alias fish_usr_paths='echo $fish_user_paths | tr " " "\n" | nl'
+alias testnvim="NVIM_APPNAME=testnvim nvim"
+alias yanvim="NVIM_APPNAME=nvimdots nvim"
 
 
 set -x PATH "$HOME/.local/bin" $PATH
