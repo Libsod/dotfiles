c6f10f69ae0d5d30ea9878ce48e6f749f869b937
qwislet <linuxuser.zlmug@slmail.me>
2024-04-12 20:24:16 +0400

    fix(nvim): ts-autotag wasn't working

--- nvim/lua/configs/ts-autotag.lua
+++ nvim/lua/configs/ts-autotag.lua
@@ -1,15 +1,15 @@
-return function()
-  require("nvim-ts-autotag").setup {
-    filetypes = {
-      "html",
-      "astro",
-      "svelte",
-      "javascript",
-      "typescript",
-      "javascriptreact",
-      "typescriptreact",
-      "vue",
-      "xml",
-    },
-  }
-end
+local options = {
+  filetypes = {
+    "html",
+    "astro",
+    "svelte",
+    "javascript",
+    "typescript",
+    "javascriptreact",
+    "typescriptreact",
+    "vue",
+    "xml",
+  },
+}
+
+return require("nvim-ts-autotag").setup(options)
