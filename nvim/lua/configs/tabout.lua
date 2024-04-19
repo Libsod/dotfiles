9f8ebda51df4b92c5481b6ba64ca3bf8f8d17f7b
qwislet <linuxuser.zlmug@slmail.me>
2024-04-20 00:47:03 +0400

    chore(nvim): minor changes in tab behavior & tabout plugin

--- nvim/lua/configs/tabout.lua
+++ nvim/lua/configs/tabout.lua
@@ -1,10 +1,10 @@
 local options = {
   tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
   backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
-  act_as_tab = true, -- shift content if tab out is not possible
+  act_as_tab = false, -- shift content if tab out is not possible
   act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-  default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-  default_shift_tab = "<C-d>", -- reverse shift default action,
+  default_tab = "", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+  default_shift_tab = "", -- reverse shift default action,
   enable_backwards = true, -- well ...
   completion = false, -- if the tabkey is used in a completion pum
   tabouts = {
