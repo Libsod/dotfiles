1be6c46a7fbe330a558d4e7b46659462079b2fbb
qwislet <linuxuser.zlmug@slmail.me>
2024-04-29 22:42:29 +0400

    fix(nvim): fixed rust lsp doesn't working

--- nvim/lua/configs/lsp/rustaceanvim.lua
+++ nvim/lua/configs/lsp/rustaceanvim.lua
@@ -1,6 +1,6 @@
-local on_attach = require("configs.lspconfig").on_attach
-local on_init = require("configs.lspconfig").rust_analyzer_on_init
-local capabilities = require("configs.lspconfig").capabilities
+local on_attach = require("configs.lsp.lspconfig").on_attach
+local on_init = require("configs.lsp.lspconfig").rust_analyzer_on_init
+local capabilities = require("configs.lsp.lspconfig").capabilities
 
 vim.g.rustaceanvim = {
   tools = {
@@ -20,6 +20,81 @@ vim.g.rustaceanvim = {
             group = "module",
           },
           prefix = "self",
+        },
+        cargo = {
+          extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+          extraArgs = { "--profile", "rust-analyzer" },
+          features = "all",
+        },
+        rustfmt = {
+          extraArgs = { "+nightly" },
+        },
+        assist = {
+          expressionFillDefault = "default",
+        },
+        inlayHints = {
+          typeHints = { enable = true },
+          bindingModeHints = { enable = true },
+          closingBraceHints = {
+            minLines = 11,
+          },
+          closureCaptureHints = { enable = true },
+          closureReturnTypeHints = {
+            enable = "with_block",
+          },
+          closureStyle = "rust_analyzer",
+          discriminantHints = {
+            enable = "fieldless",
+          },
+          parameterHints = {
+            enable = true,
+          },
+          rangeExclusiveHints = {
+            enable = true,
+          },
+          renderColons = false,
+          expressionAdjustmentHints = {
+            mode = "prefer_postfix",
+            enable = "reborrow",
+          },
+          lifetimeElisionHints = {
+            enable = "skip_trivial",
+          },
+        },
+        lens = {
+          run = { enable = false },
+        },
+        completion = {
+          fullFunctionSignatures = { enable = true },
+        },
+        procMacro = { enable = true },
+        typing = {
+          autoClosingAngleBrackets = { enable = true },
+        },
+        semanticHighlighting = {
+          operator = {
+            specialization = { enable = true },
+          },
+          punctuation = {
+            separate = {
+              macro = {
+                bang = true,
+              },
+            },
+            enable = true,
+          },
+        },
+        workspace = {
+          symbol = {
+            search = {
+              kind = "all_symbols",
+            },
+          },
+        },
+        signatureInfo = {
+          documentation = {
+            enable = false,
+          },
         },
       },
     },
